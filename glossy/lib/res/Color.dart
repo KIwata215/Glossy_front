bf33c4f9585038d5d2a6fe31f1387ce4d585310f
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2025-11-14 10:20:07 +0900

    add:使う色をまとめるファイルを作成しました

---
+++ glossy/lib/res/Color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors{
+  AppColors._();
+  //メインカラーの緑
+  static const Color customgreen = Color(0xFF1C331D);
+  //メインカラーの薄紫
+  static const Color custompurple = Color(0xFFE3D0E2);
+}
