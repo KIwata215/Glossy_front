2b685f617ea065b62acc56c61a7d1550934879b4
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2025-11-17 16:11:39 +0900

    update:コードを更新しました

---
+++ glossy/lib/ui/login_screen/Login_Screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:glossy/component/Textformfield.dart';
+import 'package:glossy/res/Color.dart';
+
+class LoginScreen extends StatefulWidget{
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
+  final TextEditingController email_controller = TextEditingController();
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Stack(
+        children: [
+          // 左上の緑の円
+          Positioned(
+            top: -100.h,
+            left: -80.w,
+            child: Container(
+              width: 250.w,
+              height: 250.h,
+              decoration: BoxDecoration(
+                color: AppColors.customgreen,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+
+          // 右上の薄紫の円
+          Positioned(
+            top: 50.h,
+            right: -40.w,
+            child: Container(
+              width: 180.w,
+              height: 180.w,
+              decoration: BoxDecoration(
+                color: AppColors.custompurple,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+
+          // 右下の緑の円
+          Positioned(
+            bottom: -60.h,
+            right: -40.w,
+            child: Container(
+              width: 240.w,
+              height: 240.w,
+              decoration: BoxDecoration(
+                color: AppColors.customgreen,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          // 左下の薄紫の円
+          Positioned(
+            bottom: -60.h,
+            left: -40.w,
+            child: Container(
+              width: 180.w,
+              height: 180.w,
+              decoration: BoxDecoration(
+                color: AppColors.custompurple,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                SizedBox(height: 125.h),
+                Image.asset(
+                  'assets/images/glossy_logo.png',
+                ),
+                SizedBox(height: 10),
+                //Email_Textformfieldの呼び出し
+                Email_Textformfield(
+                  controller: email_controller, 
+                  labelText: "メールアドレス", 
+                  width: 300.w, 
+                ),
+              ],
+            )
+          ),
+        ],
+      ) 
+    );
+  }
+}
