2bef52e94ddb77577b7b8523bb4e5a81b171c2a7
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2025-11-17 16:11:17 +0900

    update:色を追加しました

--- glossy/lib/res/Color.dart
+++ glossy/lib/res/Color.dart
@@ -6,4 +6,5 @@ class AppColors{
   static const Color customgreen = Color(0xFF1C331D);
   //メインカラーの薄紫
   static const Color custompurple = Color(0xFFE3D0E2);
+  static const Color customblack = Color(0xFF000000);
 }
