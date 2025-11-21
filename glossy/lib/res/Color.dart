d75b821b0dc3ff495d5940cc8f46c43e1e8e47d3
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-11-21 14:43:08 +0900

    update：color.dartに色を追加

--- glossy/lib/res/Color.dart
+++ glossy/lib/res/Color.dart
@@ -1,9 +1,11 @@
 import 'dart:ui';
 
-class AppColors{
+class AppColors {
   AppColors._();
   //メインカラーの緑
   static const Color customgreen = Color(0xFF1C331D);
   //メインカラーの薄紫
   static const Color custompurple = Color(0xFFE3D0E2);
+  // 投稿画面の背景の黒
+  static const Color customblack = Color(0xFF2B2B2B);
 }
