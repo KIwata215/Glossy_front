3585544e08d4a74d69e8ea984062962af91c451f
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-11-21 14:43:37 +0900

    画面ごとにテキストの内容を変えれるようにした

--- glossy/lib/component/PostHeader.dart
+++ glossy/lib/component/PostHeader.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:glossy/res/Color.dart';
 
 class PostHeader extends StatelessWidget implements PreferredSizeWidget {
   final Color backgroundColor;
-  const PostHeader({super.key, this.backgroundColor = const Color(0xFF222222)});
+  final String titleText;
+  final String rightText;
+  final Color textColor;
+  final VoidCallback? onRightTap;
+  const PostHeader({
+    super.key,
+    this.backgroundColor = const Color(0xFF2B2B2B),
+    this.titleText = "新規投稿",
+    this.rightText = "次へ",
+    this.textColor = AppColors.customgreen,
+    this.onRightTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +26,29 @@ class PostHeader extends StatelessWidget implements PreferredSizeWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            /// ←戻るボタン
             GestureDetector(
               onTap: () => Navigator.pop(context),
-              child: const Icon(
-                Icons.arrow_back_ios,
-                color: Colors.white,
-                size: 20,
-              ),
+              child: Icon(Icons.arrow_back_ios, color: textColor, size: 20),
             ),
-            const Text(
-              "新規投稿",
+
+            /// 中央タイトル
+            Text(
+              titleText,
               style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
+                color: textColor,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
 
-            const Text(
-              "次へ",
-              style: TextStyle(color: Colors.white, fontSize: 16),
+            /// 右「次へ」ボタン
+            GestureDetector(
+              onTap: onRightTap,
+              child: Text(
+                rightText,
+                style: TextStyle(color: textColor, fontSize: 16),
+              ),
             ),
           ],
         ),
