9f07b0d5b11d9cd82ab93da2782dccce0614b39c
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2025-11-25 14:14:40 +0900

    ログインボタンのサイズを変更しました、Googleログインのボタンを追加しました

--- glossy/lib/component/Button.dart
+++ glossy/lib/component/Button.dart
@@ -17,7 +17,7 @@ class LoginButton extends StatelessWidget {
       onPressed: onPressed,
       style: TextButton.styleFrom(
         backgroundColor: AppColors.customgreen,
-        minimumSize: Size(250.w,60.h), 
+        minimumSize: Size(250.w,50.h), 
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30.r),
         ),
@@ -28,6 +28,43 @@ class LoginButton extends StatelessWidget {
             color: Colors.white,
             fontWeight: FontWeight.bold,
             fontSize: 24.sp,
+        ),
+      ),
+    );
+  }
+}
+
+class GoogleLoginButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  //コンストラクタで受け取る
+  const GoogleLoginButton({
+    Key? key,
+    required this.onPressed,      
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      icon: Image.asset(
+        'assets/images/google_icon.jpeg',
+        height: 24.h,
+        width: 24.w,
+      ),
+      onPressed: onPressed, 
+      label: const Text(
+        "Googleでログイン",
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),  
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        minimumSize: Size(250.w,50.h),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.r),
+
         ),
       ),
     );
