383650d38ff5b8ade71140bcd0e0e6b2e0642b73
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2025-11-27 11:21:15 +0900

    update:ドロップダウンメニューを追加しました

--- glossy/lib/ui/register_screen/Register_Screen.dart
+++ glossy/lib/ui/register_screen/Register_Screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:glossy/component/Button.dart';
+import 'package:glossy/component/Dropdownmenu.dart';
 import 'package:glossy/component/Textformfield.dart';
 import 'package:glossy/res/Color.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,6 +14,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController username_controller = TextEditingController();
   final TextEditingController email_controller = TextEditingController();
   final TextEditingController password_controller = TextEditingController();
+  String? selectedgender;
+  String? selectedhairtype;
+  String? selectedishairdresser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +24,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
         children: [
            // 左上の緑の円
           Positioned(
-            top: -290.h,
+            top: -300.h,
             left: -140.w,
             child: Container(
               width: 400.w,
@@ -88,12 +93,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
           ),
           Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 100.h),
                 Image.asset(
                   'assets/images/glossy_logo.png',
                 ),
-                
                 Text(
                   "新規登録",
                   style: GoogleFonts.poppins(
@@ -102,7 +106,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 10.h),
                 Username_Textformfield(
                   controller: username_controller, 
                   width: 300.w, 
@@ -117,11 +120,65 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   controller: password_controller, 
                   width: 300.w
                 ), 
+                SizedBox(height: 10.h),
+                Row(
+                  children: [
+                    SizedBox(width: 50.w),
+                    Dropdownmenu(
+                      onChanged: (value){
+                        setState(() {
+                          selectedgender = value;
+                        });
+                      },
+                      labelText: '性別',
+                      items: ['男性', '女性', 'その他'],
+                      hint: '性別',
+                      width: 90.w,
+                    ),
+                    SizedBox(width: 50.w),
+                    Dropdownmenu(
+                      onChanged: (value){
+                        setState(() {
+                          selectedhairtype = value;
+                        });
+                      },
+                      labelText: '髪質',
+                      items: ['直毛', 'くせ毛', 'その他','パーマ','縮毛矯正'],
+                      hint: '髪質',
+                      width: 90.w,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.h),
+                Row(
+                  children: [
+                    SizedBox(width: 50.w),
+                    Dropdownmenu(
+                      onChanged: (value){
+                        setState(() {
+                          selectedishairdresser = value;
+                        });
+                      },
+                      items: ['はい', 'いいえ'],
+                      hint: "", 
+                      width: 200.w, 
+                      labelText: 'あなたは美容師ですか？',
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                RegisterButton(
+                  onPressed: (){
+                    //⭐︎Todo 新規登録処理
+                    print('性別:$selectedgender');
+                    print('髪質:$selectedhairtype');
+                    print('美容師ですか？:$selectedishairdresser');
+                  }
+                ),    
               ]
             ),
           )
         ],
-        
       )
     );
   }
