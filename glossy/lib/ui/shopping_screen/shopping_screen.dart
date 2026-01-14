6e57b9434cac0c4392d3f86e083c7417ef47cc1e
yannmaoki <2220050@ecc.ac.jp>
2026-01-14 15:55:07 +0900

    update:グリッドでの細かい設定

--- glossy/lib/ui/shopping_screen/shopping_screen.dart
+++ glossy/lib/ui/shopping_screen/shopping_screen.dart
@@ -71,18 +71,28 @@ class ShoppingScreen extends StatelessWidget {
 
             // ---------- 商品一覧グリッド（3列） ----------
             Expanded(
-              child: ItemGrid(items: testItems),
+              child: ItemGrid(
+                items: testItems,
+                onItemTap: (item) {
+                  Navigator.pushNamed(
+                    context,
+                    '/itemdetail',
+                    arguments: item,
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-
-      bottomNavigationBar: BottomAppBarCustom(
-        selectedIndex: 3,
-        onTap: (index) {
-          // TODO: 遷移処理
+      bottomNavigationBar: SafeArea(
+        child: BottomAppBarCustom(
+          selectedIndex: 3,
+          onTap: (index) {
+            // TODO: 遷移処理
           AppRouter.navigate(context, 3, index);
-        },
+          },
+        ),
       ),
     );
   }
