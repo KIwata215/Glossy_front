8f962f3a003ce44b76aa9a6fb05347fcd1d49798
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2026-01-25 18:50:12 +0900

    add:発表用の仮データ

---
+++ glossy/lib/data/dummy_data.dart
@@ -0,0 +1,78 @@
+import 'package:glossy/models/video_post.dart';
+
+final ladiesPosts = [
+  Post(
+    videoPath: 'assets/video/sample1_ledies.mp4',
+    userName: 'alice',
+    description: 'かわいいハーフアップのやり方',
+    likeCount: 120,
+  ),
+  Post(
+    videoPath: 'assets/video/sample2_ledies.mp4',
+    userName: 'mika',
+    description: '今日のおすすめの髪のセットのやり方',
+    likeCount: 98,
+  ),
+  Post(
+    videoPath: 'assets/video/sample3_ledies.mp4',
+    userName: 'saki',
+    description: '簡単外巻きのやり方',
+    likeCount: 75,
+  ),
+  Post(
+    videoPath: 'assets/video/sample4_ledies.mp4',
+    userName: 'mika',
+    description: '簡単な髪の巻き方',
+    likeCount: 120,
+  ),
+  Post(
+    videoPath: 'assets/video/sample5_ledies.mp4',
+    userName: 'alex',
+    description: '忙しい朝に簡単まとめ髪',
+    likeCount: 45,
+  ),
+];
+
+final followPosts = [
+  Post(
+    videoPath: 'assets/video/sample2.mp4',
+    userName: 'mike',
+    description: 'シースルーハーフアップのやり方',
+    likeCount: 89,
+  ),
+  Post(
+    videoPath: 'assets/video/sample3_ledies.mp4',
+    userName: 'saki',
+    description: '簡単外巻きのやり方',
+    likeCount: 75,
+  ),
+  Post(
+    videoPath: 'assets/video/sample3.mp4',
+    userName: 'alex',
+    description: '耳掛けセンターパートのセットのやり方',
+    likeCount: 250,
+  ),
+  Post(
+    videoPath: 'assets/video/sample5_ledies.mp4',
+    userName: 'alex',
+    description: '忙しい朝に簡単まとめ髪',
+    likeCount: 45,
+  ),
+
+];
+
+final mensPosts = [
+
+  Post(
+    videoPath: 'assets/video/sample2.mp4',
+    userName: 'mike',
+    description: 'シースルーハーフアップのやり方',
+    likeCount: 89,
+  ),
+  Post(
+    videoPath: 'assets/video/sample3.mp4',
+    userName: 'alex',
+    description: '耳掛けセンターパートのセットのやり方',
+    likeCount: 45,
+  ),
+];
